ef2b597b6c1f05733d866fc265aa05ae3fb84183
dhruv mishra <dhruv35m@gmail.com>
2023-12-05 17:23:53 +0530

    featured section

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import styles from "./featured.module.css";
+import Image from "next/image";
 
 const Featured = () => {
-  return <div className={styles.container}>Featured</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Hey, Dhruv!</b> Discover my stories and creative ideas.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src="/p1.jpeg" className={styles.image} alt="" fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic, minus.
+          </h1>
+          <p className={styles.postDesc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+            repellat accusamus maxime inventore molestias quas. Aut libero
+            consectetur, quaerat alias vel distinctio, nesciunt atque provident
+            laborum dolores laboriosam voluptatem. Earum!
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
